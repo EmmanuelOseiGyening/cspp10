23094690c752363fa39445fc9d2db85e269f9832
Emmanuel Osei Gyening <emmanueloseigyening@uagateway.org>
2016-10-27 16:16:19 +0000

    Worked On lab

--- Unit3/emanog_fizzbuzz.py
+++ Unit3/emanog_fizzbuzz.py
@@ -1,24 +1,11 @@
-# final = ""
-# inp = input("Enter a number or exit: ")
-# while(inp != "exit"):
-#     final = final + inp + " "
-#     inp = input("Enter a number or exit: ")
-# print(final)
-
-# num = input("Enter a number: ")
-# for num in list(range(1,1000,1)):
-#     if num % 3 or num == "3":
-#         print("fizz")
-#     elif num % 5 or num == "5":
-#         print("buzz")
-#     elif num % 3 and num % 5:
-#         print("fizzbuzz")
-
 num = int(input("Enter a number: "))
-for x in list(range(num,100)):
-    if x % 3 == 0 or x == "3":
+int = int(num)
+for x in range(1,num + 1):
+    if x % 3 == 0:
         print("fizz")
-    elif x % 5 == 0 or x == "5":
+    elif x % 5 == 0:
         print("buzz")
-    elif x % 3 and x % 5:
+    elif x % 3 == 0 and x % 5 == 0:
         print("fizzbuzz")
+    else:
+        print(x)
