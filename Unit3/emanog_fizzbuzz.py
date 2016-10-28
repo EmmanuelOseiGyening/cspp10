5deda36ee7c9639339477b81c715263339669ff3
Emmanuel Osei Gyening <emmanueloseigyening@uagateway.org>
2016-10-28 16:15:37 +0000

    Finished Lab

--- Unit3/emanog_fizzbuzz.py
+++ Unit3/emanog_fizzbuzz.py
@@ -1,11 +1,14 @@
 num = int(input("Enter a number: "))
 int = int(num)
 for x in range(1,num + 1):
-    if x % 3 == 0:
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzbuzz")
+    
+    elif x % 3 == 0:
         print("fizz")
+
     elif x % 5 == 0:
         print("buzz")
-    elif x % 3 == 0 and x % 5 == 0:
-        print("fizzbuzz")
+   
     else:
         print(x)
