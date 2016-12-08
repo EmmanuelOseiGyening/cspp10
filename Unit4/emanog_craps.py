7d4affefb59321d052bfc0a760cd27440b3a1e25
Emmanuel Osei Gyening <emmanueloseigyening@uagateway.org>
2016-12-08 17:15:59 +0000

    Worked on Craps Lab

--- Unit4/emanog_craps.py
+++ Unit4/emanog_craps.py
@@ -1,13 +1,16 @@
 import random
 
+def instructions():
+    
 def get_player_bet():
-    bet = int(input("How much would you lime to bet?: "))
-
+    bet = int(input("How much would you like to bet?"))
+    return bet
+    
 def dice_roll():
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
     dice_sum = dice1 + dice2 
-    print("You rolled {} {}".format(dice1,dice2))
+    return ("You rolled {} and {}. The sum is {}".format(dice1,dice2,dice_sum))
 
 def first_roll(dice_sum):
     if (dice_sum == 7) or (dice_sum == 11):
@@ -17,18 +20,19 @@ def first_roll(dice_sum):
     else:
         return dice_sum
 
+def get_or_lose_money(bet,dice_sum):
+    if (dice_sum == 7) or (dice_sum == 11):
+        bank = bet + bet
+    elif (dice_sum == 2) or (dice_sum == 3) or (dice_sum == 12):
+        bank = bet - bet 
+    else:
+        return ("It is time for Phase Three: Point Number")
+
+def craps():
+    print ("Welcome to The Wonderful Game of Craps")
+    print (" ")
     
-# def roll_result(bet,dice_sum):
-#     new_amount = 0
-#     if dice_sum == 2:
-#         new_amount = bet - bet
-#     elif dice_sum == 3:
-#         new_amount = bet - bet
-#     elif dice_sum == 12:
-#         new_amount = bet - bet
-#     elif dice_sum == 7:
-#         new_amount = bet + bet
-#     elif dice_sum == 11:
-#         new_amount = bet + bet
-#     else:
-#         point_number(dice_sum)
+    bet = get_player_bet()
+    dice_sum = dice_roll()
+    
+craps()
