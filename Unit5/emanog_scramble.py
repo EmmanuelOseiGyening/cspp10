b609f79e222ab3cdaaa6bf3cf5817b8b6a10f5ae
Emmanuel Osei Gyening <emmanueloseigyening@uagateway.org>
2017-02-03 17:15:15 +0000

    Finished psuedo code for the first function and started coding in testing.py

--- Unit5/emanog_scramble.py
+++ Unit5/emanog_scramble.py
@@ -6,12 +6,28 @@ import random
 # [4] Single out the first and last character from the string ---> You can do this by using string indexing/slicing to get the certain characters you need from the string
 # [4a] You can find the length of he string using len() to see how many ketters are in the string
 # [4b] You can then use string slicing to only get the characters in between the first and last index
-# [4c] Then you would need to set the new slicied string equal to the original variable
+# [4b] Then you would need to set the new slicied string equal to the original variable
 #                                       OR
 # [4c] You can use string slicing or indexing to remove the first and last index and only leave the remaining characters
 # You can do [4] before [3] or [3] before [4]
 # [5] Scramble the remaining characters that are in the string ---> You can do this by using the list.shuffle() code
 # [6] Print the shuffled string
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # [1] Make a function for the certain part of the code you are working on (def scramble_phase())
 # [2] Make a variable that holds the phrase
+# [3] Turn the phrase into a list <--- You can do this by using list(variable)
